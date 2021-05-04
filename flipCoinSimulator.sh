8eaf0d53b04b39ba798b22bb2ab1fa62dc3d9b60
dada4747 <adsurerahul96@gmail.com>
2021-05-04 22:44:51 +0530

    ADDED:toal no of head or tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,27 @@
 #! /bin/bash -x
+chead=0
+ctail=0
 
-flip=$(((RANDOM % 2) + 1))
-if [[ $flip -eq 1 ]]
-then
-	echo "winner is tail"
-elif [[ $flip -eq 2 ]]
-then
-	echo "winner is head"
-else
-	echo "getting error"
-fi
+
+while [ $chead -le 21 ] || [ $ctail -le 21 ]
+do
+                flip=$(((RANDOM % 2) + 1))
+
+                if [[ $flip -eq 1 ]]
+                then
+                        chead=$(( $chead + 1 ))
+#			continue
+
+
+                elif [[ $flip -eq 2 ]]
+                then
+                        ctail=$(( $ctail + 1 ))
+#                	continue
+		else
+                        echo "getting error"
+                fi
+done
+
+
+echo "head is: $chead"
+echo "tail is: $ctail"
