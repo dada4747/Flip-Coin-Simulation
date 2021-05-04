8f588afb5551ec22b269c60b9ac77609a59e4f3a
dada4747 <adsurerahul96@gmail.com>
2021-05-04 22:56:47 +0530

    ADDED:play 21 time check win

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,26 +2,43 @@
 chead=0
 ctail=0
 
+cwin1=0
+cwin2=0
 
-while [ $chead -le 21 ] || [ $ctail -le 21 ]
-do
+ while [ $chead -le 21 ] || [ $ctail -le 21 ]
+        do
                 flip=$(((RANDOM % 2) + 1))
-
                 if [[ $flip -eq 1 ]]
                 then
                         chead=$(( $chead + 1 ))
-#			continue
 
+                        if [[ $chead -eq 21 ]]
+                        then
+                                cwin1=$(($cwin2 + 1))
+                                break
+                        fi
 
                 elif [[ $flip -eq 2 ]]
                 then
                         ctail=$(( $ctail + 1 ))
-#                	continue
-		else
+                        if [[ $ctail -eq 21 ]]
+                        then
+                                cwin2=$(($cwin1 + 1))
+                                break
+                        fi
+                else
                         echo "getting error"
                 fi
-done
+        done
 
+
+
+
+
+echo "round $i"
 
 echo "head is: $chead"
 echo "tail is: $ctail"
+
+echo "head win $cwin1 time  "
+echo "tail is win $cwin2 time "
